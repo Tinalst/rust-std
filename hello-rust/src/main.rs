f78322bf74244373ce7d0e261cb2b8167d0197ab
tinalst <lst_tina@163.com>
2021-10-09 18:51:42 +0800

    update

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -570,3 +570,20 @@
 //
 //     println!("{:?}", book_hashmap.get(&1)); // Some("daladala")
 // }
+
+// fn prints_three_things(vector: Vec<i32>) {
+//     if vector.len() != 3 {
+//         panic!("must 3 len")
+//     }
+//
+//     println!("{},{},{}", vector[0], vector[1], vector[2])
+// }
+//
+// fn main() {
+//     let my_vec = vec![8,9,10];
+//     prints_three_things(my_vec);
+// }
+
+fn main() {
+
+}
